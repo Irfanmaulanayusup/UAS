4d3d4d8b5b127c3fb4fda224a4333104fb07770b
Irfanmaulanayusup <irfanyusup318@gmail.com>
2025-01-07 05:14:41 -0800

    Add files via upload

---
+++ data.py
@@ -0,0 +1,12 @@
+# Kelas untuk merepresentasikan data mahasiswa
+class DataMahasiswa:
+    def __init__(self):
+        self.daftar_mahasiswa = []
+
+    def tambah(self, nama, nilai):
+        """Menambah data mahasiswa."""
+        self.daftar_mahasiswa.append({'nama': nama, 'nilai': nilai})
+
+    def get_data(self):
+        """Mengembalikan daftar mahasiswa."""
+        return self.daftar_mahasiswa
